175fdac903463ed85f6e23302cf18ee1ba91b1c2
PradeepMuruganC <pradeepmuruganc@gmail.com>
2023-10-31 13:08:19 +0530

    commit2

---
+++ statementcontinue.java
@@ -0,0 +1,16 @@
+class statementcontinue{
+    public static void main(String args[]){
+        aa:
+        for(int i=0;i<5;i++){
+            bb:
+            for(int j=0;j<5;j++)
+            {
+                if(i==3&&j==3)
+                {
+                    continue aa;
+                }
+                System.out.println(i+" "+j);
+            }
+        }
+    }
+}
